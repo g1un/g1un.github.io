7e9a7cec6e9e1f6b8df84ffd2d10822f56e97bb9
g1un <serboff@gmail.com>
2019-11-11 09:58:36 +0300

    Initial

---
+++ precache-manifest.85d8745ad838598b612b601c5bcd2398.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81cefb4f0f697351454104047079aef1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "11024387315e6fcc3496",
+    "url": "/static/css/main.b1e3d495.chunk.css"
+  },
+  {
+    "revision": "bc1054ca086232f46b3d",
+    "url": "/static/js/2.90bcc498.chunk.js"
+  },
+  {
+    "revision": "11024387315e6fcc3496",
+    "url": "/static/js/main.46221096.chunk.js"
+  },
+  {
+    "revision": "a3a8af9a1f11f3074157",
+    "url": "/static/js/runtime-main.c0ff1062.js"
+  }
+]);
